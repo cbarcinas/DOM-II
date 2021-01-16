ca6368f5020c2de9ee95486f7a2e26edc5a50262
Craig Barcinas <cbarcinas@live.com>
2021-01-15 21:03:17 -0800

    added image 2 rotation and h1 color change

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-// Your code goes here
+// Rotate image
+const advImg = document.querySelector("#adventure-img");
+
+advImg.addEventListener("click", (event) => {
+  advImg.style.transform = "rotate(25deg)";
+});
+
+// advImg.addEventListener(‘dblclick’, () => {
+//     if (event.type === ‘dblclick’) {
+//         bottomImg.style.transform = “scaleX(-1)“;
+//     }
+// });
+
+// Change h1 text Color
+const headingColor = document.querySelector(".logo-heading");
+
+headingColor.addEventListener("mouseover", (e) => {
+  headingColor.style.color = "red";
+});
+
+headingColor.addEventListener("mouseout", (e) => {
+  headingColor.style.color = "black";
+});
