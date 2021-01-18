197815bb06d04059dab8148360588e49f2fde140
Craig Barcinas <cbarcinas@live.com>
2021-01-17 16:22:42 -0800

    added paragraph animations using gsap 3.0

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
+// use keydown escape to remove bus img
+const busImg = document.querySelector("#bus-img");
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    busImg.remove();
+  }
+});
+
 // Rotate image ========================================
 
 const bodyImg1 = document.querySelector("#img-1");
 const bodyImg2 = document.querySelector("#img-2");
 
-gsap.from("img", { opacity: 1, duration: 0.5, y: -50 });
+gsap.from("img", {
+  opacity: 1,
+  duration: 2.5,
+  x: 50,
+  ease: "bounce.easeInOut",
+});
 
 function rotate(event) {
   event.target.style.transform = "rotate(-5deg)";
@@ -17,12 +31,6 @@ bodyImg1.addEventListener("mouseout", rotateReverse);
 bodyImg2.addEventListener("mouseover", rotate);
 bodyImg2.addEventListener("mouseout", rotateReverse);
 
-// advImg.addEventListener(‘dblclick’, () => {
-//     if (event.type === ‘dblclick’) {
-//         bottomImg.style.transform = “scaleX(-1)“;
-//     }
-// });
-
 // Change h1 text Color ===================================
 const headingColor = document.querySelector(".logo-heading");
 
@@ -33,3 +41,23 @@ headingColor.addEventListener("mouseover", (e) => {
 headingColor.addEventListener("mouseout", (e) => {
   headingColor.style.color = "black";
 });
+
+// paragraph slide in ======================================
+
+// const paraText1 = document.querySelector(".para-1");
+
+gsap.from(".para-1", {
+  opacity: 0,
+  duration: 2.5,
+  x: -100,
+  ease: "bounce.easeInOut",
+});
+
+gsap.from(".para-2", {
+  opacity: 0,
+  duration: 2.5,
+  x: 100,
+  ease: "bounce.easeInOut",
+});
+
+// onscroll animation ======================================
